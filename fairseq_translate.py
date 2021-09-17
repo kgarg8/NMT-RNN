5f6fbe56afc931fc05204ad98b0e61dd53f45b2d
KrishnaGarg <krishnakantgarg@gmail.com>
2021-09-17 18:13:54 -0500

    Automate decoding

--- fairseq_translate.py
+++ fairseq_translate.py
@@ -1,37 +1,50 @@
 # Use pre-trained models FAIRSEQ and translate
-import torch, pdb
+import torch, argparse, pdb
 from tqdm import tqdm
 
-# English to German
-en2de = torch.hub.load('pytorch/fairseq', 'conv.wmt17.en-de', tokenizer='moses', bpe='subword_nmt')
-en2de.eval()
-en2de.cuda()
+def main(args):
+    
+    # Prepare kwargs
+    kwargs = None
+    if args.sampling:
+        kwargs = {'sampling': True, 'sampling_topk': args.sampling_topk, 'sampling_topp': args.sampling_topp}
+    elif args.beam:
+        kwargs = {'beam': args.beam}
 
-file1 = open('data/wmt16_en_de/test.en', 'r')
-Lines = file1.readlines()
-print('Testing samples = ', len(Lines))
+    # Load pre-trained model
+    en2de = torch.hub.load('pytorch/fairseq', 'conv.wmt17.en-de', tokenizer='moses', bpe='subword_nmt')
+    en2de.eval()
+    en2de.cuda()
 
-batch_size = 1000
-batches = len(Lines) // batch_size
-if len(Lines) % batch_size != 0:
-    batches += 1
+    # Open test file
+    test_file = open('data/wmt16_en_de/test.en', 'r')
+    Lines = test_file.readlines()
+    print('Testing samples = ', len(Lines))
+    print(kwargs)
 
-file2 = open('data/wmt16_en_de/pred.txt', 'w')
-for i in tqdm(range(batches)):
-    if (i+1)*batch_size < len(Lines):
-        y = batch_size
-    else:
-        y = len(Lines) - i*batch_size
-    pred = en2de.translate(Lines[i*batch_size : i*batch_size+y])
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topk=-1)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topk=1)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topk=10)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topk=50)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topk=100)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topp=0.5)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topp=0.8)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], sampling=True, sampling_topp=0.9)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], beam=1)
-    # pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], beam=5)
-    for item in pred:
-        file2.write("%s\n" % item)
+    batch_size = 1000
+    batches = len(Lines) // batch_size
+    if len(Lines) % batch_size != 0:
+        batches += 1
+
+    # Generate predictions
+    filename='data/wmt16_en_de/pred_topk_{}_topp_{}_beam_{}.txt'.format(args.sampling_topk, args.sampling_topp, args.beam)
+    pred_file = open(filename, 'w')
+    for i in tqdm(range(batches)):
+        if (i+1)*batch_size < len(Lines): y = batch_size
+        else:                             y = len(Lines) - i*batch_size
+        
+        pred = en2de.translate(Lines[i*batch_size : i*batch_size+y], **kwargs)
+        for item in pred:
+            pred_file.write("%s\n" % item)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sampling" , default=False, action='store_true')
+    parser.add_argument("--sampling_topk" , default=0, type=int)
+    parser.add_argument("--sampling_topp" , default=0, type=float)
+    parser.add_argument("--beam" , default=1, type=int)
+
+    args = parser.parse_args()
+    main(args)
